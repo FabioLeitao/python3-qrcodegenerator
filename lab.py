ac412c24955c37e090a3868d6f81a703bafd5660
Fabio Leitao <fabio.tleitao@gmail.com>
2024-09-28 05:21:49 -0300

    Aceitando parametros de --Help para texto de ajuda e --Zoom para ampliação da imagem gerada

--- lab.py
+++ lab.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
-import os, stat, shutil
+import os, getopt, sys, stat, shutil
 from datetime import datetime
 import segno
 
 # Preparativos e variaveis
+zoom_imagem = 6
 lista_etiquetas = "./lista.txt"
 pasta_etiquetas = "./etiquetas_" + datetime.now().strftime('%Y-%m-%d')
+
+# Remove 1st argument from the list of command line arguments
+argumentList = sys.argv[1:]
+
+# Options
+options = "hz:"
+
+# Long options
+long_options = ["Help", "Zoom="]
+
+try: 
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+
+    # Checking each argument
+    for currentArgument, currentValue in arguments:
+        if currentArgument in ("-h", "--Help"):
+            print("\n./lab.py [options]\n\n Options:\n    -h | --Help   Mostra esta ajuda\n    -z | --Zoom=   Indica zoom da imagem resultante (padrão é 6)\n\n  É necessária presença de arquivo lista.txt contento string de 12 catacteres no formato padrão T1R-T-NNNNNN para gerar etiquetas coerentes")
+            quit()
+        elif currentArgument in ("-z", "--Zoom"):
+            print(("Utilizando valor de zoom resultante (%s)") % (currentValue))
+            zoom_imagem = (currentValue)
+except getopt.error as err:
+    # Output error, and return with an error code
+    print(str(err))
 
 # Confere existencia de lista e de pasta para salvar etiquetas
 if os.path.exists(lista_etiquetas):
@@ -22,7 +48,7 @@ if os.path.exists(lista_etiquetas):
         qrcode = segno.make_qr(texto_etiqueta)
         qrcode.save(
                 arquivo_etiqueta,
-                scale=5,
+                scale=zoom_imagem,
                 )
     etiquetas.close
     print("OK - Imagens de etiquetas geradas com sucesso")
