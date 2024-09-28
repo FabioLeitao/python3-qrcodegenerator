9ef2d6703c501dc45dd8cdccf171d31aa04767d0
Fabio Leitao <fabio.tleitao@gmail.com>
2024-09-28 04:12:25 -0300

    Preparando o ambiente para automacao das pastas

--- lab.py
+++ lab.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-
+import os
+from datetime import datetime
+import shutil
 import segno
 
-qrcode = segno.make_qr("T1R-L-000001")
+# Preparativos e variaveis
+texto_etiqueta = "T1R-L-000001"
+pasta_etiquetas = "./etiquetas_" + datetime.now().strftime('%Y-%m-%d')
+arquivo_etiqueta = pasta_etiquetas + "/" + texto_etiqueta + ".png"
+
+# Confere existencia de pasta para salvar etiquetas
+if os.path.exists(pasta_etiquetas):
+    shutil.rmtree(pasta_etiquetas)
+os.makedirs(pasta_etiquetas,exist_ok=True)
+
+# Cria etiqueta(s)
+qrcode = segno.make_qr(texto_etiqueta)
 qrcode.save(
-    "T1R-L-000001_qrcode.png",
+    arquivo_etiqueta,
     scale=5,
 )
